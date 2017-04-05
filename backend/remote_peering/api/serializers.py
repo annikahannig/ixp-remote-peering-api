1da80e3ecc08f09e79ddb9eff9e56a1926b544b8
Eleni Fragkiadaki <eleni@linx.net>
2017-04-05 15:43:34 +0100

    Additional serializers

--- backend/remote_peering/api/serializers.py
+++ backend/remote_peering/api/serializers.py
@@ -7,6 +7,26 @@ from remote_peering import models
 class AsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.As
-        fields = ('number', 'created_at')
+        fields = ('number', 'created_at', 'member_set')
+
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Member
+        fields = ('asn', 'name')
+        depth = 2
+
+
+class IxpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Ixp
+        fields = ('name', 'members', 'locations', 'peeringdb_id')
+        depth = 2
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Location
+        fields = ('continent', 'country', 'city')
 
 
