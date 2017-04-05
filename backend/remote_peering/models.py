a2a1d778b1d890780000a7df6f2433a5ab7cb576
Matthias Hannig <matthias@hannig.cc>
2017-04-05 14:01:03 +0200

    created basic model

--- backend/remote_peering/models.py
+++ backend/remote_peering/models.py
@@ -1,6 +1,61 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+
+class As(models.Model):
+    number = models.IntegerField(unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Location(models.Model):
+    continent = models.CharField(max_length=80)
+    country = models.CharField(max_length=80)
+    city = models.CharField(max_length=80)
+
+    ixps = models.ManyToManyField('ixp')
+
+    class Meta:
+        unique_together = ('continent', 'country', 'city',)
+
+
+class Ixp(models.Model):
+    name = models.CharField(max_length=80)
+    members = models.ManyToManyField('member')
+    locations = models.ManyToManyField(Location)
+
+    peeringdb_id = models.IntegerField(unique=True)
+
+
+class Member(models.Model):
+
+    asn = models.ForeignKey(As, null=False, blank=False)
+    name = models.CharField(max_length=80, null=True, blank=True)
+
+    ixps = models.ManyToManyField(Ixp)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Ip(models.Model):
+    address = models.CharField(max_length=100)
+    version = models.IntegerField(default=6)
+
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
+    member = models.ForeignKey(Member)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class IpMetric(models.Model):
+    ip = models.ForeignKey(Ip)
+    median_rtt = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
