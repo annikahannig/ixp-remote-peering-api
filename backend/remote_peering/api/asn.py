cb4d8c0daa953c9e0ae596dd6a2567c2a587278d
Eleni Fragkiadaki <eleni@linx.net>
2017-04-05 14:52:48 +0100

    Initial commit of the retrieve single asn

--- backend/remote_peering/api/asn.py
+++ backend/remote_peering/api/asn.py
@@ -1,15 +1,25 @@
 
 from rest_framework import viewsets, response
+from remote_peering import models
+from django.core.exceptions import *
 
 
 class AsnViewSet(viewsets.ViewSet):
     def list(self, request):
-        return response.Response({
-            'status': 'OK'
-        })
+        return response.Response(models.As.objects.all())
 
     def retrieve(self, request, pk=None):
-        return response.Response({
-            'pk': pk
-        })
+        try:
+            asn = models.As.objects.get(pk=pk)
+        except MultipleObjectsReturned:
+            return response.Response({
+                "status": 400,
+                "message": "Multiple records identified"
+            }, 400)
+        except ObjectDoesNotExist:
+            return response.Response({
+                "status": 404,
+                "message": "Object does not exist"
+            }, 404)
 
+        return response.Response({"status": 200, "data": asn})
