242f5a3068b99b70eed5797fa80e20548d1b1fd4
Matthias Hannig <matthias@hannig.cc>
2017-04-06 16:30:14 +0200

    added additional date filtering

--- backend/remote_peering/api/ip_metrics.py
+++ backend/remote_peering/api/ip_metrics.py
@@ -13,33 +13,54 @@ class IpMetricsViewSet(viewsets.ViewSet):
     """
     Retrieve and filter IP metrics.
 
-    ### QueryParameters:
-    * `?date=<date>`
+    ### Date Querying
+    * `?date=<date>` e.g. 2016-08-01T00:00:00Z
+    * `?day=<1-31>`
+    * `?month=<1-12>`
+    * `?year=<yyyy>`
+
+    ### Query Parameters
     * `?ip=<ip address>[,<ip address>...]`
     * `?asn=<as number>[,<as number>...]`
     * `?median_rtt_<lte|gte>=<float>`
 
-    ###
+    ### Pagination
     * `?limit=<number>`
     * `?start=<number>`
     """
 
     def list(self, request):
+
+        # Date filtering
         created_at = request.query_params.get('date')
+        date_day = request.query_params.get('day')
+        date_month = request.query_params.get('month')
+        date_year = request.query_params.get('year')
+
+
         ip_address = utils.params_list(request, 'ip')
         asn = utils.params_list(request,'asn')
         median_rtt_lte = request.query_params.get('median_rtt_lte')
         median_rtt_gte = request.query_params.get('median_rtt_gte')
 
+        # Limiting
         start = int(request.query_params.get('start', 0))
         limit = request.query_params.get('limit', None)
-
         end = int(limit) + start if limit else None
 
         query_list = []
 
         if created_at:
             query_list.append(Q(created_at=created_at))
+
+        if date_day:
+            query_list.append(Q(created_at__day=date_day))
+
+        if date_month:
+            query_list.append(Q(created_at__month=date_month))
+
+        if date_year:
+            query_list.append(Q(created_at__year=date_year))
 
         if ip_address:
             query_list.append(Q(ip__address__in=ip_address))
