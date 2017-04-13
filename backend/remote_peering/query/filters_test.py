5b7f441fa516491205887d094004bc2ec02d569e
Matthias Hannig <matthias@hannig.cc>
2017-04-13 17:30:52 +0200

    added datetime testing support

--- backend/remote_peering/query/filters_test.py
+++ backend/remote_peering/query/filters_test.py
@@ -6,6 +6,8 @@ Test filter generation
 
 from __future__ import unicode_literals
 
+from datetime import datetime
+from django.utils import timezone
 from remote_peering.query import filters
 
 def test_filters_whitelist():
@@ -39,4 +41,29 @@ def test_filters_whitelist():
 
     assert whitelist_params['baz'] == 42
     assert whitelist_params['baz__lt'] == 42
+
+
+def test_date_parsing():
+    """Test date handling in queryparams"""
+    params = {
+        'foo': '2016-2-15',
+        'bar_start': '2016-10-15 23:15:10',
+        'bar_end': '2016-11-15 23:15:10',
+    }
+
+    schema = {
+        'foo': (datetime, 'lt', 'lte'),
+        'bar_start': (range, datetime, 'gt'),
+        'bar_end': (range, datetime, 'lte'),
+    }
+
+    whitelist_params = filters.whitelist_params(params, schema)
+
+    assert whitelist_params['foo'] == timezone.utc.localize(
+        datetime(2016, 2, 15))
+    assert whitelist_params['bar_start__gt'] == timezone.utc.localize(
+        datetime(2016, 10, 15, 23, 15, 10))
+    assert whitelist_params['bar_end__lte'] == timezone.utc.localize(
+        datetime(2016, 11, 15, 23, 15, 10))
+
 
