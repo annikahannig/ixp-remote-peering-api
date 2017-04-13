28108105dc984d9bc1339bb2a52d079c10e6449f
Matthias Hannig <matthias@hannig.cc>
2017-04-13 12:56:02 +0200

    updated test

--- backend/remote_peering/query/filters_test.py
+++ backend/remote_peering/query/filters_test.py
@@ -16,12 +16,15 @@ def test_filters_whitelist():
         'foos': '23,42',
         'bar__icontains': 'test',
         'bar__baz': 'foo',
+        'baz': '42',
+        'baz__lt': '42',
     }
 
     schema = {
         'foo': (float, ),
         'foos': ([int], ),
         'bar': (str, 'icontains'),
+        'baz': (int, 'lt'),
     }
 
     whitelist_params = filters.whitelist_params(params, schema)
@@ -34,5 +37,6 @@ def test_filters_whitelist():
     assert whitelist_params['foos__in'] == [23, 42]
     assert whitelist_params['bar__icontains'] == 'test'
 
-
+    assert whitelist_params['baz'] == 42
+    assert whitelist_params['baz__lt'] == 42
 
